9cb56c5974d049d38c579e6c29e5a7665fbe563e
ViachMoz <v.mozulevsky@gmail.com>
2020-09-28 13:01:49 +0300

    sanitize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [ personsState, setPersons ] = useState({
+        persons: [
+            {name: "Stan", age: 26},
+            {name: "Steph", age: 23 },
+            {name: "Marge", age: 35 }
+        ]
+    })
+
+    const changeNameHandler = () => {
+        setPersons({
+            persons: [
+                {name: "Math", age: 26},
+                {name: "John", age: 23 },
+                {name: "Lilly", age: 35 }
+            ]
+        })
+    }
+
+    return (
+        <div className="App">
+            <h1>Hi, i'm your new friend, react app.</h1>
+            <button onClick={changeNameHandler}>Change name!</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: is sitting near a couch.</Person>
+            <Person name={personsState.persons[2].name} age={personsState .persons[2].age} />
+        </div>
+    );
 }
 
 export default App;
+
+
+
